e88c8d74d76a8f814beb604a64f7fb1624d51a9c
afruzaakter <afruzaaktersonia1995@gmail.com>
2022-06-14 03:49:21 +0600

    add get and project data load

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -12,8 +12,12 @@ app.use(express.json());
 
 
 
+
+
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4h8ai.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -23,21 +27,26 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try{
         await client.connect();
-        const projectsCollection = client.db('portfolio-projects').collection('projects');  
+        const projectsCollection=client.db('portfolio-projects').collection('projects');  
      
         //get
         app.get('/project', async(req, res) =>{
          const query = {};
          console.log(query);
          const cursur = projectsCollection.find(query);
-         const projects = await cursur.toArray();
-         res.send(projects);
+         const project = await cursur.toArray();
+         res.send(project);
 
         })
 
         //post
 
-        app.post('/projects', async(req, res))
+        app.get('/project/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const project = await projectsCollection.findOne(query);
+            res.send(project);
+        });
 
 
     }
